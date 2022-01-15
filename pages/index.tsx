a9ba3b9f384fda77014b508e723baded2d184665
jay <dj.min43@gmail.com>
2022-01-15 20:33:44 +0900

    add cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from 'next'
 import Main from '../components/Main'
 import Layout from '../components/layout/Layout'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const Home: NextPage = () => {
+
+  useEffect(() => {
+    console.log('hello world')
+  }, [])
+
+  const getResult = async () => {
+    const result = await axios.get('http://localhost:80/all')
+    console.log(result, 'get result')
+    return result
+  }
+
+  getResult()
+
   return (
    <Layout>
      <Main />
