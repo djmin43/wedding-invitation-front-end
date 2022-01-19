2b289b524cd4ce3232c5d911354388bc726b76c6
jay <dj.min43@gmail.com>
2022-01-19 21:10:01 +0900

    feat: add og image and title

--- components/App.tsx
+++ components/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled'
+import Head from 'next/head'
 
 const App = () => {
   return <Main>
+    <Head>
+      <title>한종윤 결혼식</title>
+      <meta property="og:image" content="https://mindongjoon.blob.core.windows.net/general/hj-pic.jpeg" key="hj-image" />
+    </Head>
     <Header>
       <p>한종윤</p>
       <p>이혜진</p>
