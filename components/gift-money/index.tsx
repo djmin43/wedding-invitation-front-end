be6a6b73ded6febe0dd25996e8c496a248680f56
jay <dj.min43@gmail.com>
2022-02-13 20:27:58 +0900

    chore: clean

--- components/gift-money/index.tsx
+++ components/gift-money/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import * as S from '@styled'
-// import Accordion from '@mui/material/Accordion'
-// import AccordionSummary from '@mui/material/AccordionSummary'
-// import AccordionDetails from '@mui/material/AccordionDetails'
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 const GiftMoney = () => {
   return (
@@ -13,12 +9,7 @@ const GiftMoney = () => {
         <p>참석이 어려우신 분들은</p>
         <p>축하의 마음을 전달해주세요.</p>
       </S.ParagraphWrapper>
-      {/* <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          asjdkflajsdfl
-        </AccordionSummary>
-        <AccordionDetails>백만원</AccordionDetails>
-      </Accordion> */}
+
     </S.WhiteCardContainer>
   )
 }
